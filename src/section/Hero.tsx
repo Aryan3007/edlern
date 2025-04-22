d1541c3121cc265c8971a8eed190ee838717da76
ARYAN TYAGI <aryantyagi111s@gmail.com>
2025-04-23 00:11:28 +0530

    fixing buttons

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -19,12 +19,12 @@ export default function HeroSection() {
               <p className="text-base font-semibold tracking-wider text-lime-600 uppercase">A social media for learners</p>
               <h1 className="mt-4 text-5xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-7xl"> Build your best community starting from here.</h1>
               <p className="mt-4 text-base text-black lg:mt-8 sm:text-lg">Meet and communicate with the best people to run projects, events or other activities in a more effective and fun way.</p>
-             <div className="flex flex-col gap-4 sm:flex-row">
-               <button className="inline-flex shadow-xl gap-2 items-center px-6 py-3 mt-6 font-semibold text-white transition-all duration-200 bg-gradient-to-br from-lime-500 to-lime-600 rounded-full lg:mt-16 hover:bg-lime-500/80 focus:bg-lime-500/60" role="button">
+             <div className="flex flex-col mt-6 gap-4 sm:flex-row">
+               <button className="inline-flex shadow-xl gap-2 w-fit items-center px-6 py-3  font-semibold text-white transition-all duration-200 bg-gradient-to-br from-lime-500 to-lime-600 rounded-full lg:mt-16 hover:bg-lime-500/80 focus:bg-lime-500/60" role="button">
                 Create Community
                 <ChevronRight />
               </button>
-                <Link to={"/discover"} className="inline-flex gap-2 items-center px-6 py-3 mt-6 font-semibold  transition-all duration-200 border-lime-500 border text-lime-600 rounded-full lg:mt-16 " role="button">
+                <Link to={"/discover"} className="inline-flex gap-2 w-fit items-center px-6 py-3  font-semibold  transition-all duration-200 border-lime-500 border text-lime-600 rounded-full lg:mt-16 " role="button">
                 Explore
                 <Compass />
               </Link>        
